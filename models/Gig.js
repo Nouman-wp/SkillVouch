e5753229ed54b2288cdd4122223e1cef8b572fcb
Nouman <noumanpm@gmail.com>
2025-03-23 00:17:44 +0530

    New Update v4

--- models/Gig.js
+++ models/Gig.js
@@ -1,13 +1,42 @@
 const mongoose = require('mongoose');
 
 const gigSchema = new mongoose.Schema({
-    title: String,
+    title: {
+        type: String,
+        required: true
+    },
     description: String,
     price: Number,
+
+    location: {
+        type: String,  // e.g., "Delhi, India"
+        default: "Remote"
+    },
+
+    mode: {
+        type: String,
+        enum: ['On-site', 'Remote', 'Hybrid'],
+        default: 'Remote'
+    },
+
+    skillsRequired: [{
+        type: String  // Example: ["JavaScript", "React", "Node.js"]
+    }],
+
+    deadline: {
+        type: Date  // Optional deadline for the gig
+    },
+
+    image: {
+        type: String,
+        default: '/images/default-job.png'  // Optional gig image for UI
+    },
+
     createdAt: {
         type: Date,
         default: Date.now
     },
+
     postedBy: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
